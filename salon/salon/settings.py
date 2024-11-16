08ce9a3ce6008e13602669b43430b5f4b9e9fe77
momogodi2000 <yvangodimomo@gmail.com>
2024-11-16 14:09:32 +0100

    online haire salon

--- salon/salon/settings.py
+++ salon/salon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@7j^!9**zckakcb)@!un(bnbv22t)n%r%rwwfh%prcjbonb7g&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['salon.onrender.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
