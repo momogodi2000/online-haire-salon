d27d61b5251145e265c9c8d88ac63e699b28e1dd
momogodi2000 <yvangodimomo@gmail.com>
2024-11-16 21:59:15 +0100

    fixe bug

--- salon/salon/settings.py
+++ salon/salon/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,9 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'assets'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompresseManifestStaticFileStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
