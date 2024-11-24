f9d2a15257dcf70417015152583844af96c89556
momogodi2000 <yvangodimomo@gmail.com>
2024-11-24 16:50:36 +0100

    new update outdir

--- salon/alex/urls.py
+++ salon/alex/urls.py
@@ -17,6 +17,11 @@ urlpatterns = [
     path('flyer/', views.flyer, name='flyer'),
 
     distill_path('', views.home, name='home', distill_func=get_index),
+    distill_path('about/', views.about, name='about', distill_func=get_index),
+    distill_path('services/', views.services, name='services', distill_func=get_index),
+    distill_path('booking/', views.booking, name='booking', distill_func=get_index),
+    distill_path('flyer/', views.flyer, name='flyer', distill_func=get_index),
+
 
 
 
